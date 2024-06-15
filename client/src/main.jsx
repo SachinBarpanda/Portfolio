b5ef6f77060fa4a568d705deb41585357827c4f3
Sachin <sachinongithub@gmail.com>
2024-06-15 19:06:41 +0530

    back to normal

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,12 +4,10 @@ import App from './App.jsx'
 import './index.css'
 import store from './redux/store.js'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    
+    <App />
   </Provider>,
 )
