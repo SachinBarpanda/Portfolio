646e7f715c8f583c0adb6e5bbab4ed194b5fdbc3
Sachin <sachinongithub@gmail.com>
2024-06-15 16:06:56 +0530

    removed the trouble code

--- index.js
+++ index.js
@@ -10,14 +10,7 @@ app.use(express.json());
 app.use(cors())
 app.use("/api/portfolio/",portfolioRoute);
 
-app.use(express.static(path.join(__dirname, 'build')));
 
-
-app.get('*', (req, res) => {
-
-  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-
-});
 
 
 
