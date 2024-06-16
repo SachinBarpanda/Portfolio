b13f3c84d0042a800dcd2066a444fe4ff9def80c
Sachin <sachinongithub@gmail.com>
2024-06-16 13:40:12 +0530

    config

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -44,13 +44,7 @@ function App() {
     </div>
     :
     <ConfigProvider
-    theme={{
-      // 1. Use dark algorithm
-      algorithm: theme.darkAlgorithm,
 
-      // 2. Combine dark algorithm and compact algorithm
-      // algorithm: [theme.darkAlgorithm, theme.compactAlgorithm],
-    }}
   >
       <Outlet/>
     </ConfigProvider>
